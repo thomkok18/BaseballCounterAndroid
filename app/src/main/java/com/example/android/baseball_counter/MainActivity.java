2fbbc0767d450250504c85c7ac6e1407f536d8c1
Thom Kok <tkok@roc-dev.com>
2017-02-07 12:59:19 +0100

    update

--- app/src/main/java/com/example/android/baseball_counter/MainActivity.java
+++ app/src/main/java/com/example/android/baseball_counter/MainActivity.java
@@ -3,65 +3,33 @@ package com.example.android.baseball_counter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0, scoreTeamB = 0;
+    TextView team;
+    CheckBox out1, out2, out3;
+    CheckBox strike1, strike2, strikeout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /**
-     * Increase the score for Team A by 4 points.
-     */
-    public void addFourForTeam(View view) {
-        TextView team = (TextView) findViewById(R.id.teamAanSlag);
-        if(team.getText().toString().equals("Team A")) {
-            scoreTeamA += 4;
-            displayForTeamA(scoreTeamA);
-        } else if(team.getText().toString().equals("Team B")) {
-            scoreTeamB += 4;
-            displayForTeamB(scoreTeamB);
-        }
-    }
-
-    /**
-     * Increase the score for Team A by 3 points.
-     */
-    public void addThreeForTeam(View view) {
-        TextView team = (TextView) findViewById(R.id.teamAanSlag);
-        if(team.getText().toString().equals("Team A")) {
-            scoreTeamA += 3;
-            displayForTeamA(scoreTeamA);
-        } else if(team.getText().toString().equals("Team B")) {
-            scoreTeamB += 3;
-            displayForTeamB(scoreTeamB);
-        }
-    }
-
-    /**
-     * Increase the score for Team A by 2 points.
-     */
-    public void addTwoForTeam(View view) {
-        TextView team = (TextView) findViewById(R.id.teamAanSlag);
-        if(team.getText().toString().equals("Team A")) {
-            scoreTeamA += 2;
-            displayForTeamA(scoreTeamA);
-        } else if(team.getText().toString().equals("Team B")) {
-            scoreTeamB += 2;
-            displayForTeamB(scoreTeamB);
-        }
+        team = (TextView) findViewById(R.id.teamAanSlag);
+        out1 = (CheckBox) findViewById(R.id.out1);
+        out2 = (CheckBox) findViewById(R.id.out2);
+        out3 = (CheckBox) findViewById(R.id.out3);
+        strike1 = (CheckBox) findViewById(R.id.strike1);
+        strike2 = (CheckBox) findViewById(R.id.strike2);
+        strikeout = (CheckBox) findViewById(R.id.strikeout);
     }
 
     /**
      * Increase the score for Team A by 1 point.
      */
     public void addOneForTeam(View view) {
-        TextView team = (TextView) findViewById(R.id.teamAanSlag);
         if(team.getText().toString().equals("Team A")) {
             scoreTeamA++;
             displayForTeamA(scoreTeamA);
@@ -77,17 +45,16 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
+        if (out1.isChecked() || out2.isChecked() || out3.isChecked() || strike1.isChecked() || strike2.isChecked() || strikeout.isChecked()) {
+            out1.setChecked(false);
+            out2.setChecked(false);
+            out3.setChecked(false);
+            strike1.setChecked(false);
+            strike2.setChecked(false);
+            strikeout.setChecked(false);
+        }
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
-    }
-
-    /**
-     * Displays the given score for both teams.
-     */
-    public void displayReset(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.reset);
-        scoreView.setText(String.valueOf(score));
-
     }
 
     /**
